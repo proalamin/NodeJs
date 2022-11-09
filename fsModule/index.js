bb4a50e0b83ee1f174c3942ec620590caaaba5f8
proalamin-247 <pro.alamin247@gmail.com>
2022-11-09 13:04:58 +0600

    async and sync write file

--- fsModule/index.js
+++ fsModule/index.js
@@ -5,20 +5,48 @@ var server=http.createServer(function (req, res){
 
     if(req.url="/"){
 
-        //Asy
+        //Asy read file
         // fs.readFile('Home.html', function(error, data){
         //     res.writeHead(200, {'Content-Type': 'Text/html'});
         //     res.write(data);
         //     res.end();
         // });
 
-        //Syn
-        let myData= fs.readFileSync('Home.html');
-        res.writeHead(200, { 'Content-Type': 'Text/html' });
-        res.write(myData);
-        res.end();
-    }
+        // //Syn read file
+        // let myData= fs.readFileSync('Home.html');
+        // res.writeHead(200, { 'Content-Type': 'Text/html' });
+        // res.write(myData);
+        // res.end();
 
+
+        //Asy write file
+        fs.writeFile('demo.txt', 'Hello World', function(error){
+
+            if(error){
+                res.writeHead(200, { 'Content-Type': 'Text/html' });
+                res.write("file write fail");
+                res.end();
+            }
+            else{
+                res.writeHead(200, { 'Content-Type': 'Text/html' });
+                res.write("file write successfull");
+                res.end();
+            }
+        })
+
+        //Sync write file
+        let error=fs.writeFileSync('demoSync.txt', 'Hello World');
+            if(error){
+                res.writeHead(200, { 'Content-Type': 'Text/html' });
+                res.write("file write fail");
+                res.end();
+            }
+            else{
+                res.writeHead(200, { 'Content-Type': 'Text/html' });
+                res.write("file write successfull");
+                res.end();
+            }
+    }
 
 });
 server.listen(5001);
